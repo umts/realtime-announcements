0ba4561e52830208416eff634569dc1af7b1eb38
David Faulkenberry <david.faulkenberry@gmail.com>
2017-08-02 10:07:51 -0400

    Some "tests"

--- spec/announcer_spec.rb
+++ spec/announcer_spec.rb
@@ -5,4 +5,35 @@ require 'spec_helper'
 include Announcer
 
 describe Announcer do
+  describe 'run' do
+    before :each do
+      expect_any_instance_of(Announcer).to receive(:set_query_stops)
+      expect_any_instance_of(Announcer).to receive(:set_interval)
+      expect_any_instance_of(Announcer).to receive(:new_departures)
+        .and_return :departures
+      expect_any_instance_of(Announcer).to receive(:cached_departures)
+        .and_return :cached_departures
+      expect_any_instance_of(Announcer)
+        .to receive(:departures_crossed_interval)
+        .with(:departures, :cached_departures)
+        .and_return departures_to_announce
+      expect_any_instance_of(Announcer).to receive(:cache_departures)
+        .with :departures
+    end
+    context 'with departures to announce' do
+      let(:departures_to_announce) { [:departure_to_announce] }
+      it 'makes announcements' do
+        expect_any_instance_of(Announcer).to receive(:make_announcement)
+          .with(:departure_to_announce)
+        run
+      end
+    end
+    context 'with no departures to announce' do
+      let(:departures_to_announce){ [] }
+      it 'makes no announcements' do
+        expect_any_instance_of(Announcer).not_to receive(:make_announcement)
+        run
+      end
+    end
+  end
 end
