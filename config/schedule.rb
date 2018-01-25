1acbcfc73e4ccc7a69d40b5970c7002f05a5432e
David Faulkenberry <david.faulkenberry@gmail.com>
2018-01-25 15:11:43 -0500

    Auto-update via cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,9 +2,15 @@
 
 env :PATH, ENV['PATH']
 
+job_type :command, 'cd :path && :task'
 job_type :rake, 'cd :path && bundle exec rake :task'
 
 # Every minute after 5am, until 3am
 every '* 0-2,5-23 * * *' do
   rake 'announcer:run'
+end
+
+# Every day at 4am, git pull
+every :day, at: '4:00 am' do
+  command 'git pull; bundle exec whenever -w'
 end
